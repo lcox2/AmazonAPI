dfa892694645c3baae7c30a4ce757bff1c165d80
jharper4 <jharper4@wvup.edu>
2016-09-27 19:26:00 -0400

    plz work =/

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 console.log('Lets get down to business');
+
+console.log('hello billy!');
+
+console.log('awssecretHash');
+
+var AWS = require('aws-sdk'); 
+
+var s3 = new AWS.S3(); 
+
+ s3.createBucket({Bucket: 'myBucket'}, function() {
+
+  var params = {Bucket: 'myBucket', Key: 'myKey', Body: 'Hello!'};
+
+  s3.putObject(params, function(err, data) {
+
+      if (err)       
+
+          console.log(err)     
+
+      else       console.log("Successfully uploaded data to myBucket/myKey");   
+
+   });
+
+});
