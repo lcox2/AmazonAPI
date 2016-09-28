c52167cc7d94075477b8019206b3a351731ff6f7
jharper4 <jharper4@wvup.edu>
2016-09-27 20:42:03 -0400

    fix

--- index.js
+++ index.js
@@ -43,22 +43,3 @@ opHelper.execute('ItemSearch', {
     }).catch((err) => {
         console.error("Something went wrong! ", err);
     });
-var AWS = require('aws-sdk'); 
-
-var s3 = new AWS.S3(); 
-
- s3.createBucket({Bucket: 'myBucket'}, function() {
-
-  var params = {Bucket: 'myBucket', Key: 'myKey', Body: 'Hello!'};
-
-  s3.putObject(params, function(err, data) {
-
-      if (err)       
-
-          console.log(err)     
-
-      else       console.log("Successfully uploaded data to myBucket/myKey");   
-
-   });
-
-});
