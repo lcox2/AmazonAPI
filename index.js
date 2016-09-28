93f7bedfb5ba079b83bcd6dfbafec3ea295d0d4c
lcox2 <lcox2@wvup.edu>
2016-09-27 20:24:28 -0400

    IT WORKS !!!

--- index.js
+++ index.js
@@ -2,23 +2,41 @@ console.log('hello billy!');
 
 console.log('awssecretHash');
 
-var AWS = require('aws-sdk');
-AWS.config.loadFromPath('./config.json')
+// var AWS = require('aws-sdk');
+// AWS.config.loadFromPath('./config.json')
 
-var s3 = new AWS.S3(); 
+// var s3 = new AWS.S3(); 
 
- s3.createBucket({Bucket: 'myBucket'}, function() {
+//  s3.createBucket({Bucket: 'myBucket'}, function() {
 
-  var params = {Bucket: 'myBucket', Key: 'myKey', Body: 'Hello!'};
+//   var params = {Bucket: 'myBucket', Key: 'myKey', Body: 'Hello!'};
 
-  s3.putObject(params, function(err, data) {
+//   s3.putObject(params, function(err, data) {
 
-      if (err)       
+//       if (err)       
 
-          console.log(err)     
+//           console.log(err)     
 
-      else       console.log("Successfully uploaded data to myBucket/myKey");   
+//       else       console.log("Successfully uploaded data to myBucket/myKey");   
 
-   });
+//    });
 
+var util = require('util'),
+    OperationHelper = require('apac').OperationHelper;
+
+var opHelper = new OperationHelper({
+    awsId:     'AKIAI7LCAK3FMX7NN47Q',
+    awsSecret: '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',
+    assocId:   'lcox2-20'
 });
+
+opHelper.execute('ItemSearch', {
+    'SearchIndex': 'Books',
+    'Keywords': 'harry potter',
+    'ResponseGroup': 'ItemAttributes,Offers'
+    }).then((response) => {
+        console.log("Results object: ", response.results);
+        console.log("Raw response body: ", response.responseBody);
+    }).catch((err) => {
+        console.error("Something went wrong! ", err);
+    });
