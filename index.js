ab29675243315ec26637c641a6278c08d20d6075
lcox2 <lcox2@wvup.edu>
2016-10-04 20:13:36 -0400

    John said brox brackets LOL

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
+console.log('Lets get down to business 2');
+
+console.log('hello billy!');
+
 console.log('awssecretHash');
+
 var amazonReturn;
+
 // var AWS = require('aws-sdk');
 // AWS.config.loadFromPath('./config.json')
 
@@ -25,18 +31,25 @@ var util = require('util'),
 var opHelper = new OperationHelper({
     awsId:     'AKIAI7LCAK3FMX7NN47Q',
     awsSecret: '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',
-    assocId:   'lcox2-20'
+    assocId:   'lcox2-20',
 });
 
 opHelper.execute('ItemSearch', {
     'SearchIndex': 'Books',
-    'Keywords': 'harry potter',
+    'Keywords': 'Hard Cover Harry Potter Sorcerers Stone' ,
     'ResponseGroup': 'ItemAttributes,Offers'
     }).then((response) => {
         console.log("Results object: ", response.results);
-        //console.log("Raw response body: ", response.responseBody);
-        amazonReturn = response.responseBody;
-        console.log("Results: ", response.responseBody);
+        var arr = [response.result.ItemSearchResponse.Items];
+        // console.log("Raw response body: ", response.responseBody);
+        amazonReturn = response.result.ItemSearchResponse.Items;
+        console.log(amazonReturn);
     }).catch((err) => {
         console.error("Something went wrong! ", err);
     });
+
+module.exports = {
+    find: function(){
+
+    }
+}
