5ca12e17fd1bb0b85310f1799cce01154143e857
lcox2 <lcox2@wvup.edu>
2016-10-04 20:14:05 -0400

    John likes his fingers smacekd

--- index.js
+++ index.js
@@ -6,24 +6,6 @@ console.log('awssecretHash');
 
 var amazonReturn;
 
-// var AWS = require('aws-sdk');
-// AWS.config.loadFromPath('./config.json')
-
-// var s3 = new AWS.S3(); 
-
-//  s3.createBucket({Bucket: 'myBucket'}, function() {
-
-//   var params = {Bucket: 'myBucket', Key: 'myKey', Body: 'Hello!'};
-
-//   s3.putObject(params, function(err, data) {
-
-//       if (err)       
-
-//           console.log(err)     
-
-//       else       console.log("Successfully uploaded data to myBucket/myKey");   
-
-//    });
 
 var util = require('util'),
     OperationHelper = require('apac').OperationHelper;
