155343b1a3597e6a073202d0a0a180e22ac9aa0d
jharper4 <jharper4@wvup.edu>
2016-11-29 19:21:07 -0500

    banans

--- amazon/finder.js
+++ amazon/finder.js
@@ -1,5 +1,8 @@
+var Q = require("q");
 var util = require('util'),
+
     OperationHelper = require('apac').OperationHelper;
+
 
 var opHelper = new OperationHelper({
     awsId: 'AKIAI7LCAK3FMX7NN47Q',
@@ -7,68 +10,93 @@ var opHelper = new OperationHelper({
     assocId: 'lcox2-20',
 });
 
-function test(search){
+
+
+
+function finder(input) {
+
     var deferred = Q.defer();
-    var success = true;
 
-    setTimeout(function(){
-         if(success)
-        {
-            deferred.resolve("Success");
+    opHelper.execute('ItemSearch', {
+        'SearchIndex': 'All',
+        'Keywords': input,
+        'ResponseGroup': 'ItemAttributes,Offers,Images'
+    }).then((response) => {
+        //var arr = response.result.ItemSearchResponse.Items.Item;
+        // console.log("Raw response body: ", response.responseBody);
+
+        
+        var arr = response.result.ItemSearchResponse.Items.Item;
+
+        function data() {
+            this.name = "Na";
+            this.id = "NA";
+            this.url = "NA";
+            this.category = "NA";
+            this.price = "NA";
+            this.imageURL= "NA";
         }
 
-        else
-        {
-            deferred.reject("Failure");
-        }
-    }, 2000)
-       
+        setTimeout(function () {
+            if (response.result.ItemSearchResponse.Items.Item[0] != undefined) {
+                var resarr = [];
+                for (var index = 0; index < arr.length; index++) {
+                    resarr.push(new data());
+                    resarr[index].name = ("Name: ", arr[index].ItemAttributes.Title);
+                    resarr[index].id = ("Id: ", arr[index].ItemAttributes.UPC);
+                    resarr[index].url = ("URL: ", arr[index].DetailPageURL);
+                    resarr[index].category = ("Category: ", arr[index].ItemAttributes.ProductGroup);
+                    if (arr[index].Offers.Offer != undefined) {
+                        resarr[index].price = ("Price: ", arr[index].Offers.Offer.OfferListing.Price.FormattedPrice );
+                    }
+                    else if(arr[index].ItemAttributes.ListPrice != undefined){
+                        resarr[index].price = ("Price: ", arr[index].ItemAttributes.ListPrice.FormattedPrice);
+                    }
+                    else{
+
+                    }
+                    
+                    if(arr[index].MediumImage != undefined)
+                    {
+                        resarr[index].imageURL = (arr[index].MediumImage.URL)
+                    }
+                    
+                    else if(arr[index].ImageSets.ImageSet[0] != undefined)
+                    {
+                        resarr[index].imageURL = (arr[index].ImageSets.ImageSet[0].MediumImage.URL)
+                    }
+                    
+                    else if(arr[index].ImageSets.ImageSet != undefined)
+                    {
+                        resarr[index].imageURL = (arr[index].ImageSets.ImageSet.MediumImage.URL)
+                    }
+                    
+                }
+
+                deferred.resolve(resarr);
+            }
+
+            else {
+                deferred.reject();
+            }  
+        }, 5000)
+
+        
+    })
 
     return deferred.promise;
 }
 
-var k = test();
-k.then(function(resp){
-debugger;
-}).catch(function(err){
-debugger;
+
+
+
+var test = finder("Tamiya")
+
+test.then(function(arr){
+    console.log(arr[0].name);
+    console.log(arr[0].price);
+}).catch(function(){
+
 })
-
-        function finder(input)
-        {
-            opHelper.execute('ItemSearch', {
-                'SearchIndex': 'Books',
-                'Keywords': input,
-                'ResponseGroup': 'ItemAttributes,Offers'
-            }).then((response) => {
-                //var arr = response.result.ItemSearchResponse.Items.Item;
-                // console.log("Raw response body: ", response.responseBody);
-                var arr = response.result.ItemSearchResponse.Items.Item;
-
-                function data() {
-                    this.name = "Na";
-                    this.id = "NA";
-                    this.url = "NA";
-                    this.category = "NA";
-                    this.price = "NA";
-            }
-                var resarr = [];
-                    for (var index = 0; index < arr.length; index++)
-                    {
-                        resarr.push(new data());
-                        resarr[index].name = ("Name: ", arr[index].ItemAttributes.Title);
-                        resarr[index].id = ("Id: ", arr[index].ItemAttributes.UPC);
-                        resarr[index].url = ("URL: ", arr[index].DetailPageURL);
-                        resarr[index].category = ("Category: ", arr[index].ItemAttributes.ProductGroup);
-                        if(arr[index].ItemAttributes.ListPrice != undefined)
-                        {
-                            resarr[index].price = ("Price: ", arr[index].ItemAttributes.ListPrice.FormattedPrice);
-                        }
-                        else
-                        {
-                            resarr[index].price = "NA";
-                        }
-                    }
-        })
-        }
+console.log("Hiy")
 
